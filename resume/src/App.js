2a8136142ece7d1a893f9935ea14000b3e3f6acb
karlaAgar <157807635+karlaAgar@users.noreply.github.com>
2025-02-06 01:31:07 +0000

    init

---
+++ resume/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/prueba';
+import Presentation from './components/prueba';
+
+function App() {
+  return (
+    <div className="App">
+        <Presentation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ resume/src/components/prueba.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css"
+const Presentation = () => {
+    return (
+        <div class="container">
+        <header>
+            <h1>Karla Agar Choque Contreas</h1>
+            <h2>Egresada en Ingeniería de Sistemas</h2>
+        </header>
+        
+        <img src="https://lh3.googleusercontent.com/pw/AP1GczO3R2SEgnOhosW-KxUASqX4yNOupy1Z1xFjti_H7k3seubUns6dUEfTwUM3DvH9Wo9vo0w_jfevSY767p27QXQ3n-0ql8vf-eK18ZSSy7E4h5UKuPfECnQm0uMpPkFNjEzLsY59BRG3kVooQ5ph3nDt=w659-h879-s-no-gm?authuser=0" alt="Foto de Karla Agar Choque Contreas" class="profile-img"/>
+        
+        <section class="info">
+            <h3>Datos Personales</h3>
+            <p><strong>Email:</strong> karla.agar777@gmail.com</p>
+            <p><strong>Teléfono:</strong> +59174303666</p>
+            <p><strong>Dirección:</strong> Zona Chojñacollo, Calle 2, Quillacollo - Cochabamba</p>
+        </section>
+        
+        <section class="perfil">
+            <h3>Perfil</h3>
+            <p>Ingeniera de Sistemas apasionada por el desarrollo de software con habilidades en programación y resolución de problemas. Comprometida con la mejora continua y el trabajo en equipo.</p>
+        </section>
+     </div>
+    )
+}
+
+export default Presentation;
